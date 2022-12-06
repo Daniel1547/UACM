8141f6a1ee36104e5b1dd39de90df20935573cdf
Dani <daniel.tynig@gmail.com>
2022-12-06 01:30:06 -0600

    mi primer commit

---
+++ UACM/UACM/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UACM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+           // InitializeComponent();
+
+            MainPage = new NavigationPage ( new MainPage());
+        }
+        public static readonly Xamarin.Forms.BindableProperty BarBackgroundColorProperty;
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UACM/UACM/Controller/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UACM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Para navegavilidad
+            btnInicioSesion.Clicked += (sender, e) =>
+            {
+                Navigation.PushAsync(new Page1());
+            };
+        }
+    }
+}
